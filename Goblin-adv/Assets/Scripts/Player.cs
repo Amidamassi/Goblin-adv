7b0f725508e39129bcf874538e5c38639ab5e185
AmiDamassi <>
2022-04-21 19:17:20 +0300

    PlayerMovement v1

---
+++ Goblin-adv/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    public int PlayerSpeed=0;
+
+    public Vector3 Playermovement;
+    void Start()
+    {
+        PlayerTransform = this.GetComponent<Transform>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Playermovement.z=+1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Playermovement.z=-1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Playermovement.x = +1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Playermovement.x=-1;
+        }
+
+        PlayerTransform.Translate(Playermovement.normalized*PlayerSpeed*Time.deltaTime);
+
+        Playermovement = new Vector3(0, 0, 0);
+    }
+}
