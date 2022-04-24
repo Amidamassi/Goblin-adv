90a5981f371000cd13615fd03120b0705826cde3
KcinKcip <fedor_pushko@mail.ru>
2022-04-24 13:08:21 +0300

    add UniTack pack
    add UniTack pack

--- Goblin-adv/Assets/Scripts/AiController.cs
+++ Goblin-adv/Assets/Scripts/AiController.cs
@@ -2,37 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class AiController : MonoBehaviour
 {
-    [SerializeField] private Transform goblin;
-    public float spawnCd;
-    public float spawnRange;
-    [SerializeField] private Transform enemy;
-    private Vector3 _randomPosition;
-    private Transform _enemyClone;
-    void Start()
-    {
-        StartCoroutine(EnemySpawner());
-    }
+  [SerializeField] private Transform goblin;
+  public float spawnCd;
+  public float spawnRange;
+  [SerializeField] private Transform enemy;
+  private Vector3 _randomPosition;
+  private Transform _enemyClone;
 
+  void Start()
+  {
+    StartSpawnEnemy();
+  }
 
-private IEnumerator EnemySpawner ()
-{
+  private async void StartSpawnEnemy()
+  {
     while (true)
     {
-        yield return new WaitForSeconds(spawnCd);
-        
-        _randomPosition = new Vector3(Random.Range(-spawnRange, spawnRange),0, Random.Range(-spawnRange, spawnRange));
-        _enemyClone = Instantiate(enemy, goblin.position + _randomPosition,Quaternion.identity);
-        EnemyBehavior enemyBehavior = _enemyClone.GetComponent(typeof(EnemyBehavior)) as EnemyBehavior;
-        enemyBehavior.ChangeTarget(goblin);
-        enemyBehavior.objectType= "enemy";
+      await UniTask.Delay(TimeSpan.FromSeconds(spawnCd).Milliseconds);
 
+      _randomPosition = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+      _enemyClone = Instantiate(enemy, goblin.position + _randomPosition, Quaternion.identity);
+      EnemyBehavior enemyBehavior = _enemyClone.GetComponent<EnemyBehavior>();
+      enemyBehavior.ChangeTarget(goblin);
+      enemyBehavior.objectType = "enemy";
     }
+  }
 }
-
-}
-
